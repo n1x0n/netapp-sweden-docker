537c086472166dfbdb41581635727345b8ee83d7
Admin User <admin@swarm-00.sto-demo.europe.netapp.com>
2017-04-06 21:03:58 +0200

    Add more info to raise_salary.sh

--- bin/raise_salary.sh
+++ bin/raise_salary.sh
@@ -19,20 +19,24 @@ do
 	PORT=$( docker service inspect $CLONE | grep "PublishedPort" | sed 's/,$//' | tail -1 | awk '{ print $NF }' )
 	MYSQL="$CONNECT_STRING $PORT"
 	echo "Making adjustments to clone \"$CLONE\" on port $PORT."
+	sleep $SLEEP
 
 	COMMAND="SELECT * FROM employees WHERE first_name = 'Niclas' AND last_name = 'Plumb';"
-	sleep $SLEEP
 	echo; echo "$COMMAND" | $MYSQL 
+        echo "  ^^^^^^ -- First find the employee number"
+	sleep $SLEEP
 
 	COMMAND="SELECT * FROM salaries WHERE emp_no = '294545';"
+	echo; echo "$COMMAND" | $MYSQL | sed 's/\(9999.*\)/\1 <-- Find the current salary/'
 	sleep $SLEEP
-	echo; echo "$COMMAND" | $MYSQL 
 
 	COMMAND="UPDATE salaries SET salary = '999999' WHERE emp_no = '294545' AND from_date = '2002-01-09';"
-	echo; echo "$COMMAND" | $MYSQL 
-	COMMAND="SELECT * FROM salaries WHERE emp_no = '294545';"
+	echo; echo "Update the current salary..." ; echo "$COMMAND" | $MYSQL 
 	sleep $SLEEP
-	echo; echo "$COMMAND" | $MYSQL 
+
+	COMMAND="SELECT * FROM salaries WHERE emp_no = '294545';"
+	echo; echo "$COMMAND" | $MYSQL | sed 's/\(9999.*\)/\1 <-- New salary stored in the database/'
+	sleep $SLEEP
 done
 
 
